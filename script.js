d7bf672b3f94d37fac97c7622ff9ee60abc3002a
Donna Clever <drc0625@aol.com>
2020-10-05 19:10:33 -0400

    Add logic to make sure the first chars match criteria specified

--- script.js
+++ script.js
@@ -2,79 +2,88 @@
 document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Various Arrays (Numbers, Special Characters, Lower case alphabet, Upper case alphabet)
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialChar = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~"];
+const alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Variable Declaration 
-var psdLength = "";
-var psdSpecialChar;
-var psdNumericChar;
-var psdUpperCase;
-var psdLowerCase;
+let pswdLength = "";
+let pswdSpecialChar;
+let pswdNumericChar;
+let pswdUpperCase;
+let pswdLowerCase;
 
 // Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
-    var psdLength = '';
+    pswdLength = '';
     
     //Prompt user to input desired character length (Validate that entry is numeric and between 8 to 128 characters).  Cancel will exit prompts.
-    while (isNaN(psdLength) || psdLength < 8 || psdLength > 128) {
-        psdLength = prompt("What length would you like the password to be? (Between 8 to 128 characters)");
+    while (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
+        pswdLength = prompt("What length would you like the password to be? (Between 8 to 128 characters)");
 
-        if (psdLength === null) {
+        if (pswdLength === null) {
             alert("Cancelling");
             return '';
         }
         else {
-            if (isNaN(psdLength) || psdLength < 8 || psdLength > 128) {
+            if (isNaN(pswdLength) || pswdLength < 8 || pswdLength > 128) {
                 alert("Password length must be between 8-128 characters Try again");
             }
         }
     }
 
     // Repeat back how many charactes the user will have  
-    alert(`Your password will have ${psdLength} characters`);
+    alert(`Your password will have ${pswdLength} characters`);
 
     // Determine parameters of password 
-    var psdSpecialChar = confirm("Click OK to confirm if you want to include special characters");
-    var psdNumericChar = confirm("Click OK to confirm if you want to include numeric characters");    
-    var psdLowerCase = confirm("Click OK to confirm if you want to include lowercase characters");
-    var psdUpperCase = confirm("Click OK to confirm if you want to include uppercase characters");
+    pswdSpecialChar = confirm("Click OK to confirm if you want to include special characters");
+    pswdNumericChar = confirm("Click OK to confirm if you want to include numeric characters");    
+    pswdLowerCase = confirm("Click OK to confirm if you want to include lowercase characters");
+    pswdUpperCase = confirm("Click OK to confirm if you want to include uppercase characters");
 
     // Loop if answer is outside the parameters 
-    while(psdUpperCase === false && psdLowerCase === false && psdSpecialChar === false && psdNumericChar === false) {
+    while(pswdUpperCase === false && pswdLowerCase === false && pswdSpecialChar === false && pswdNumericChar === false) {
         alert("You must choose at least one parameter");
-        var psdSpecialChar = confirm("Click OK to confirm if you want to include special characters");
-        var psdNumericChar = confirm("Click OK to confirm if you want to include numeric characters");    
-        var psdLowerCase = confirm("Click OK to confirm if you want to include lowercase characters");
-        var psdUpperCase = confirm("Click OK to confirm if you want to include uppercase characters");   
+        pswdSpecialChar = confirm("Click OK to confirm if you want to include special characters");
+        pswdNumericChar = confirm("Click OK to confirm if you want to include numeric characters");    
+        pswdLowerCase = confirm("Click OK to confirm if you want to include lowercase characters");
+        pswdUpperCase = confirm("Click OK to confirm if you want to include uppercase characters");   
     } 
 
-    // Merge the criteria arrays into one big array.  Could have created the above array at the top.  However, I wanted modularity.
-    var psdCharacters = []
-      
-    if (psdSpecialChar) {
-        psdCharacters = psdCharacters.concat(specialChar)
+    // Merge the criteria arrays into one big array.  Also ensure that the selected criteria shows up at least once.  This is being done at the beginning of the password.
+    let pswdCharacters = [];
+    let randomPassword = "";
+    let criteriaTotal = 0;
+
+    if (pswdUpperCase) {
+        pswdCharacters = pswdCharacters.concat(alphaUpper)
+        criteriaTotal += 1;
+        randomPassword = randomPassword + alphaUpper[Math.floor(Math.random() * alphaUpper.length)];
     }
 
-    if (psdNumericChar) {
-        psdCharacters = psdCharacters.concat(number)
-    }
-      
-    if (psdLowerCase) {
-        psdCharacters = psdCharacters.concat(alphaLower)
+    if (pswdSpecialChar) {
+        pswdCharacters = pswdCharacters.concat(specialChar)
+        criteriaTotal += 1;
+        randomPassword = randomPassword + specialChar[Math.floor(Math.random() * specialChar.length)];
     }
 
-    if (psdUpperCase) {
-        psdCharacters = psdCharacters.concat(alphaUpper)
+    if (pswdNumericChar) {
+        pswdCharacters = pswdCharacters.concat(number)
+        criteriaTotal += 1;
+        randomPassword = randomPassword + number[Math.floor(Math.random() * number.length)];
+    }
+      
+    if (pswdLowerCase) {
+        pswdCharacters = pswdCharacters.concat(alphaLower)
+        criteriaTotal += 1;
+        randomPassword = randomPassword + alphaLower[Math.floor(Math.random() * alphaLower.length)];
     }
 
     // Create random password from the concatenated array.
-    var randomPassword = ""
       
-    for (var i = 0; i < psdLength; i++) {
-        randomPassword = randomPassword + psdCharacters[Math.floor(Math.random() * psdCharacters.length)];
+    for (var i = criteriaTotal; i < pswdLength; i++) {
+        randomPassword = randomPassword + pswdCharacters[Math.floor(Math.random() * pswdCharacters.length)];
     }
     return randomPassword;
 }
@@ -82,5 +91,5 @@ function generatePassword() {
 // Write password to the #genpassword (Generated Password) input
 function writePassword() {
     var password = generatePassword();
-    document.querySelector("#genpassword").value = password;
+    document.querySelector("#genPassword").value = password;
 }
